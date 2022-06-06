e4788ba3a2747c1b0847845dfbb867cd0d0800a6
vero <vero@debian.fibertel.com.ar>
2022-06-05 22:44:35 -0300

    add conocimientos section

--- src/components/Knowledge.vue
+++ src/components/Knowledge.vue
@@ -2,56 +2,40 @@
   <div id="knowledge">
     <p class="conocimientos">~ Conocimientos</p>
     <div class="charts">
+<div class="x">
+                <div class="idioma">
+            <h2>Idioma</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Ingles</td>
+                  <td>Básico</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
       <div class="conocimientos-lenguajes">
-        <bar
-          ref="bar"
-          :chart-options="chartOptions"
-          :chart-data="chartData"
-          height="100"
-        ></bar>
-        <div class="idioma">
-          <h2>Idioma</h2>
-          <table>
-            <tbody>
-              <tr>
-                <td>Ingles</td>
-                <td>Básico</td>
-              </tr>
-            </tbody>
-          </table>
+      
+        <div class="col">
+
+          <div id="chart"></div>
         </div>
-      </div>
+        <div class="col">
+          <div id="knowledge-repo-percent"></div>
+        </div>
+      </div></div>
     </div>
   </div>
 </template>
 
 <script>
-import { Bar } from "vue-chartjs";
+import ApexCharts from "apexcharts";
 import Information from "../mock/Information";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
-
-ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-);
+import github from "../services/github";
 
 export default {
   name: "AboutKnowledge",
-  components: {
-    Bar,
-  },
+  components: {},
 
   data() {
     return {
@@ -64,33 +48,100 @@ export default {
           },
         ],
       },
-      chartOptions: {
-          type:'line',
-        responsive: true,
-        backgroundColor: "rgba(25,157,253,0.8)",
-        scales: {
-          y: {
-            max: 100,
-          },
-        },
-      },
-      donutChart: {
-        responsive: true,
-        maintainAspectRatio: false,
-        animation: {
-          animateRotate: false,
-        },
-      },
-      donutData:{
-        labels: ['skill1'],
-        datasets: [
-          {
-            backgroundColor: "rgba(25,157,253,0.8)",
-            data: [1]
-          }
-        ]
-      }
     };
+  },
+  async mounted() {
+    console.log(
+      (await github.repoPercentLanguage(Information.aboutMySelf.lastname)).map(
+        (repo) => repo.repos
+      )
+    );
+    let optionsBarLanguagesKnowLedge = {
+      title: {
+        text: "Conocimientos en cada lenguaje",
+        align: "center",
+      },
+      chart: {
+    width: '100%',
+        type: "bar",
+        toolbar: {
+          show: false,
+        },
+      },
+      series: [
+        {
+          name: "sales",
+          data: this.chartData.datasets[0].data,
+        },
+      ],
+      xaxis: {
+        categories: this.chartData.labels,
+      },
+    };
+
+    let optionPieLanguagesPercent = {
+      title: {
+        text: "Cantidad de lenguajes usados en proyectos",
+        align: "center",
+      },
+      series: (
+        await github.repoPercentLanguage(Information.aboutMySelf.lastname)
+      ).map((repo) => repo.repos),
+      labels: (
+        await github.repoPercentLanguage(Information.aboutMySelf.lastname)
+      ).map((repo) => repo.label),
+      chart: {
+        type: "donut",
+        toolbar: {
+          show: false,
+        },
+      },
+      plotOptions: {
+          pie: {
+            donut: {
+              labels: {
+                show: true,
+                name: {
+                  show: true,
+                },
+                value: {
+                  show: true,
+                  fontSize: '28px',
+                  fontFamily: 'Helvetica, Arial, sans-serif',
+                  color: undefined,
+                  offsetY: 16,
+                  formatter: function (val) {
+                    return val
+                  },
+                },
+                total: {
+                  show: true,
+                  fontSize:'28px',
+                  label: 'Repositorios',
+                  color: '#373d3f',
+                  formatter: function (w) {
+                    return w.globals.seriesTotals.reduce((a, b) => {
+                      return a + b
+                    }, 0)
+                  }
+                }
+              }
+            }
+          }
+        },
+    };
+
+    let chartBarLanguagesKnowLedge = new ApexCharts(
+      document.querySelector("#chart"),
+      optionsBarLanguagesKnowLedge
+    );
+    let chartPieLanguagesPercent = new ApexCharts(
+      document.querySelector("#knowledge-repo-percent"),
+      optionPieLanguagesPercent
+    );
+
+    chartBarLanguagesKnowLedge.render();
+    chartPieLanguagesPercent.render();
   },
 };
 </script>
@@ -100,7 +151,7 @@ export default {
   background: #199dfd;
   width: 100%;
   border-collapse: collapse;
-  margin: 40px 0px;
+  margin-bottom: 50px;
 }
 table {
   width: 100%;
@@ -122,11 +173,36 @@ tr > * {
   font-weight: bold;
 }
 .conocimientos-lenguajes {
+  margin: auto;
   width: 600px;
   height: 1000px;
 }
 
 .idioma > * {
   color: white;
+}
+.idioma > h2{
+  margin-left: 10px;
+}
+.col {
+  flex: 1;
+  align-items: center;
+}
+.col>svg{
+  display: flex;
+  align-items: center;
+
+}
+.conocimientos-lenguajes {
+  display: flex;
+  width: 1200px;
+  height: auto;
+  align-items: center;
+}
+.charts{
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- src/services/github.js
+++ src/services/github.js
@@ -1,6 +1,9 @@
 import * as axios from 'axios'
 const githubAPI = "https://api.github.com/"
 export default{
+    ifNull(repoLan){
+        return (repoLan != null) ? repoLan : "Desconocidos" 
+    },
     async getUser(user){
         return (await axios(
             {
@@ -8,5 +11,33 @@ export default{
                 url:        `${githubAPI}users/${user}`,
                 responseType: 'stream'
             })).data
-    }
+    },
+    async repoPercentLanguage(user){
+        let proyects = [];
+        (await axios(
+            {
+                method:     'get',
+                url:        `${githubAPI}users/${user}/repos?per_page=100`,
+                responseType: 'stream'
+            }
+        )).data.forEach(repo=>{
+            const labelData = this.ifNull(repo.language)
+            if(proyects.findIndex(data=>data.label == labelData) == -1){
+                proyects.push({
+                    label: labelData,
+                    repos: 1
+                })
+
+            }
+            else{
+                proyects.forEach(proyect=>{
+                    if(labelData == proyect.label){
+                        proyect.repos++
+                    }
+                })
+            }
+        });
+        return proyects
+    },
+
 }
